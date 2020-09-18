78038dfffbd921e05e0ac002f680096beffbe6e8
vagrant <vagrant@localhost.localdomain>
2020-09-18 20:18:15 +0900

    change delete button location

--- resources/views/admin/task.blade.php
+++ resources/views/admin/task.blade.php
@@ -88,12 +88,10 @@
               @foreach($titles as $title)
               <div class="titleLoop section">
                 <a href="{{ route('admin.task', ['id' => $title->id]) }}" class="list-group-item border-0 bg-light title" style="display: inline-block">
-                  {{ $title->title }}
-                </a>
-                
-                  <img class="titleIcon" src="{{ asset('image/deleteIcon.jpeg') }}" alt="delete" style="float: right; margin-left: 10px; margin-top: 12px;" data-toggle="modal" data-target="#deleteModal{{$title->id}}">
-              
-
+                  {{ $title->title }}</a>
+                <img class="titleIcon" src="{{ asset('image/deleteIcon.jpeg') }}" alt="delete" style="float: right; margin-left: 10px; margin-top: 12px;" data-toggle="modal" data-target="#deleteModal{{$title->id}}">
+                  
+                  <!-- タイトル削除のモーダル -->
                   <div id="deleteModal{{$title->id}}" class="modal fade" role="dialog">
                     <div class="modal-dialog">          
                       <!-- Modal content-->
@@ -106,8 +104,7 @@
                           <h4 class="text-center">{{$title->title}}</h4>
                           <p class="text-center my-4" style="font-size: 15px;">このTodoを削除しますか？</p>
                           <div class="text-center">
-                            <a href="{{ action('TaskController@delete', ['id' => $title->id]) }}"  class="btn btn-danger text-center" style="float: right";>削除</a>
-                            <!-- <button type="submit" class="btn btn-danger">削除</button> -->
+                            <a href="{{ action('TaskController@delete', ['id' => $title->id]) }}"  class="btn btn-danger">削除</a>
                           </div>
                           </form>
                         </div>
