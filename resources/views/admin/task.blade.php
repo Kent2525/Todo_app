2e90eb1725dcfabd4b0726a92ae945e7c26b7499
vagrant <vagrant@localhost.localdomain>
2020-09-18 20:28:57 +0900

    maked edit view for title

--- resources/views/admin/task.blade.php
+++ resources/views/admin/task.blade.php
@@ -115,9 +115,34 @@
                     </div>
                   </div>
 
-                  <img class="titleIcon" src="{{ asset('image/editIcon.jpeg') }}" alt="edit" style="float: right; margin-top: 12px;" data-toggle="modal" data-target="#myModal"> 
-                  
-              </div>
+                <img class="titleIcon" src="{{ asset('image/editIcon.jpeg') }}" alt="edit" style="float: right; margin-top: 12px;" data-toggle="modal" data-target="#editTitleModal"> 
+                <div id="editTitleModal" class="modal fade" role="dialog">
+                  <div class="modal-dialog">          
+                    <!-- Modal content-->
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title"></h4>
+                      </div>
+                      <div class="modal-body">
+                        <h4 class="text-center">タイトル変更</h4>
+                        <form action=""  method="post">
+                        <input type="hidden" name="id" value="{{$currentTitle->id}}">
+                          <div class="form-group">
+                            <input id="title" type="text" name="heading" class="form-control">
+                          </div>
+                          {{ csrf_field() }}
+                          <div class="text-center">
+                            <button type="submit" class="btn btn-primary">変更</button>
+                          </div>
+                        </form>
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">閉じる</button>
+                      </div>
+                    </div>             
+                  </div>         
+                </div>
               @endforeach
             </ul>
           </div>
