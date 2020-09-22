c7c2d76d065d6cb067595c6e2cfb571aba48697e
KobayashiTomoya <kobayashi.tomoya@moneyforward.co.jp>
2020-09-22 12:33:41 +0900

    モーダルを別ファイルに移す

---
+++ resources/views/admin/modal.blade.php
@@ -0,0 +1,46 @@
+            <!-- Modal -->
+            <div id="myModal" class="modal fade" role="dialog">
+              <div class="modal-dialog">             
+                <!-- Modal content-->
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title"></h4>
+                  </div>
+                  <div class="modal-body">
+                  @if ($errors->any())
+                      <div class="alert alert-danger">
+                          <ul>
+                              @foreach ($errors->all() as $error)
+                                  <li>{{ $error }}</li>
+                              @endforeach
+                          </ul>
+                      </div>
+                  @endif
+                    <h4 class="text-center">タイトル</h4>
+                    <form action="/admin/task/{{$title->id}}" method="POST">
+                      @csrf
+                      <div class="form-group">
+                        <label for="heading">見出し</label>
+                        <input id="heading" type="text" class="form-control" name="heading" value="{{ old('heading') }}">
+                      </div>
+                      <div class="form-group">
+                        <label for="ccontent">メモ</label>
+                        <textarea id="content" type="text" class="form-control" row="5" name="content"></textarea>
+                      </div>
+                      <div class="text-center">
+                      @method('PUT')
+                       <button type="submit" class="btn btn-primary">更新</button>
+                    </form>
+                    <form action="/admin/task/{{$title->id}}" method="POST">
+                      @csrf
+                      @method('DELETE')
+                      <button type="submit" class="btn btn-primary">削除</button>
+                    </form>
+                    </div>
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">閉じる</button>
+                  </div>
+                </div>             
+              </div>
--- resources/views/admin/task.blade.php
+++ resources/views/admin/task.blade.php
@@ -77,6 +77,7 @@
         <div class="col-9 mx-auto bg-white">
           <div class="right-title-box my-4 mx-5">
             <p class="test">右のスペース</p>
+            <a href="/task/create">新規作成</a>
             <ul class="list-group">
             @foreach($title_posts as $title)
               @foreach($title->contents as $content)
@@ -86,53 +87,8 @@
             </ul>
 
 
-            <!-- Modal -->
-            <div id="myModal" class="modal fade" role="dialog">
-              <div class="modal-dialog">             
-                <!-- Modal content-->
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <h4 class="modal-title"></h4>
-                  </div>
-                  <div class="modal-body">
-                  @if ($errors->any())
-                      <div class="alert alert-danger">
-                          <ul>
-                              @foreach ($errors->all() as $error)
-                                  <li>{{ $error }}</li>
-                              @endforeach
-                          </ul>
-                      </div>
-                  @endif
-                    <h4 class="text-center">タイトル</h4>
-                    <form action="/admin/task/{{$title->id}}" method="POST">
-                      @csrf
-                      <div class="form-group">
-                        <label for="heading">見出し</label>
-                        <input id="heading" type="text" class="form-control" name="heading" value="{{ old('heading') }}">
-                      </div>
-                      <div class="form-group">
-                        <label for="ccontent">メモ</label>
-                        <textarea id="content" type="text" class="form-control" row="5" name="content"></textarea>
-                      </div>
-                      <div class="text-center">
-                      @method('PUT')
-                       <button type="submit" class="btn btn-primary">更新</button>
-                    </form>
-                    <form action="/admin/task/{{$title->id}}" method="POST">
-                      @csrf
-                      @method('DELETE')
-                      <button type="submit" class="btn btn-primary">削除</button>
-                    </form>
-                    </div>
-                  </div>
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">閉じる</button>
-                  </div>
-                </div>             
-              </div>
-            </div>
+          @include('admin.modal')
+          </div>
  
           </div>
         </div>
