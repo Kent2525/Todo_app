2546b367beede74a3716e68913b0ee3c77ec6dff
Kent2525 <[aibo396@yahoo.co.jp]>
2020-09-24 02:25:47 +0900

    git test8

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -31,7 +31,7 @@ class TaskController extends Controller
         $titles = Auth::user()->titles()->get();
         // $titles = Title::all();
         $currentTitle = Title::find($id);
-        // git test1
+
 
         return view('admin.task.show', [
             'titles' => $titles,
