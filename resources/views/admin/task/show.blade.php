572735ba60b388e7196c60c9e0d6156812da50a8
Kent2525 <aibo396@yahoo.co.jp>
2020-09-25 01:31:54 +0900

    revised layout (include header)

--- resources/views/admin/task/show.blade.php
+++ resources/views/admin/task/show.blade.php
@@ -2,120 +2,94 @@
 @section('title','Todo一覧  ')
 
 @section('content')
+<div class="main">
+  <div class="row">
+    <div class="col-3  mx-aut">
+      <div class="left-title-box my-4 mx-5">
+        <p><a class="AddTitleModal" data-toggle="modal" data-target="#titleModal" data-user_id="">+タイトル追加</a></p>
 
-  <!-- ヘッダー -->
-  <body>
-    <nav class="navbar navbar-default bg-primary navbar-light">
-      <div class="container-fluid">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-          <a class="navbar-brand text-white" href="#">Todo</a>
+        {{-- 左側のタイトル追加モーダル --}}
+        @include('components.AddTitleModal')
+
+        <ul class="list-group">
+          @foreach($titles as $title)
+          <div class="titleLoop section">
+            <a href="{{ route('admin.task', ['id' => $title->id]) }}" class="list-group-item border-0 bg-light title" style="display: inline-block">
+              {{ $title->title }}</a>
+              
+            <img class="titleIcon" src="{{ asset('image/deleteIcon.jpeg') }}" alt="delete" style="float: right; margin-left: 10px; margin-top: 12px;" data-toggle="modal" data-target="#deleteModal{{$title->id}}">
+              
+              {{-- 左側の削除アイコンモーダル --}}
+              @include('components.DeleteTitleModal', ['title' => $title])
+
+            <img class="titleIcon editModal" src="{{ asset('image/editIcon.jpeg') }}" alt="edit" style="float: right; margin-top: 12px;" data-toggle="modal" data-target="#editTitleModal" data-title="{{$title->title}}" data-id_title="{{ $title->id }}"> 
+
+              {{-- 左側の更新アイコンモーダル --}}
+              @include('components.UpdateTitleModal')
+
+          @endforeach
         </ul>
-        <ul class="navbar-nav">
-        <li class="nav-item navbar-light">
-              <span class="text-white">{{ Auth::user()-> name }}さんページ</span>
-              <a class="btn btn-primary" href="{{ route('logout') }}"
-                onclick="event.preventDefault();
-                              document.getElementById('logout-form').submit();">
-                ログアウト
-              </a>
-
-              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                @csrf
-              </form>
-          </li>
-        </ul>
-      </div>
-    </nav>
-
-    <div class="main">
-      <div class="row">
-        <div class="col-3  mx-aut">
-          <div class="left-title-box my-4 mx-5">
-            <p><a class="AddTitleModal" data-toggle="modal" data-target="#titleModal" data-user_id="">+タイトル追加</a></p>
-
-            {{-- 左側のタイトル追加モーダル --}}
-            @include('components.AddTitleModal')
-
-            <ul class="list-group">
-              @foreach($titles as $title)
-              <div class="titleLoop section">
-                <a href="{{ route('admin.task', ['id' => $title->id]) }}" class="list-group-item border-0 bg-light title" style="display: inline-block">
-                  {{ $title->title }}</a>
-                  
-                <img class="titleIcon" src="{{ asset('image/deleteIcon.jpeg') }}" alt="delete" style="float: right; margin-left: 10px; margin-top: 12px;" data-toggle="modal" data-target="#deleteModal{{$title->id}}">
-                 
-                  {{-- 左側の削除アイコンモーダル --}}
-                  @include('components.DeleteTitleModal', ['title' => $title])
-
-                <img class="titleIcon editModal" src="{{ asset('image/editIcon.jpeg') }}" alt="edit" style="float: right; margin-top: 12px;" data-toggle="modal" data-target="#editTitleModal" data-title="{{$title->title}}" data-id_title="{{ $title->id }}"> 
-
-                  {{-- 左側の更新アイコンモーダル --}}
-                  @include('components.UpdateTitleModal')
-
-              @endforeach
-            </ul>
-          </div>
-        </div>
-        
-        {{-- 右側 --}}
-        <div class="col-9 mx-auto bg-white">
-          <div class="right-title-box my-4 mx-5">
-            <p class="test">見出し</p>
-            @if (empty($currentTitle))
-              <p>記事を選択してください。</p>
-            @else
-            
-            <ul class="list-group">
-               @foreach($currentTitle->contents as $content)
-                  <li><a class="list-group-item headingIndex heading_modal ml-2" data-toggle="modal" data-target="#myModal" data-heading="{{$content->heading}}" data-body="{{$content->body}}" data-id="{{ $content->id }}" >{{$content->heading}}
-                  </a></li>
-               @endforeach
-            </ul>
-
-            @endif
-
-            {{-- 右側の見出しクリックモーダル --}}
-            @include('components.UpdateContentModal')
-
-            <a type="button" class="btn btn-primary mt-3 text-white" data-toggle="modal" data-target="#addHeadingModal">追加</a>
-            
-            {{-- 右側の見出し追加モーダル --}}
-            @include('components.AddContentModal')
-
-          </div>
-        </div>
       </div>
     </div>
+    
+    {{-- 右側 --}}
+    <div class="col-9 mx-auto bg-white">
+      <div class="right-title-box my-4 mx-5">
+        <p class="test">見出し</p>
+        @if (empty($currentTitle))
+          <p>記事を選択してください。</p>
+        @else
+        
+        <ul class="list-group">
+            @foreach($currentTitle->contents as $content)
+              <li><a class="list-group-item headingIndex heading_modal ml-2" data-toggle="modal" data-target="#myModal" data-heading="{{$content->heading}}" data-body="{{$content->body}}" data-id="{{ $content->id }}" >{{$content->heading}}
+              </a></li>
+            @endforeach
+        </ul>
 
-    {{-- モータルへデータを送るjavascript --}}
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="//netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-    <script>
-    // heading-modal（ループしているデータ）をクリックした時に
- $('.heading_modal').on('click', function() {
-      // このdata('heading')（data-title="ドルcontent->heading"）をheadingに代入
-      var heading = $(this).data('heading');
-      // このdata('body')（data-body="ドルcontent->body"）をtitleに代入
-      var body = $(this).data('body');
+        @endif
 
-      // '#inputHeading_modal'に対してtitleを埋める
-      $('#inputHeading_modal').val(heading);
-      // '#inputBody_modal'のvalueにbodyを設定する。
-      $('#inputBody_modal').val(body);
+        {{-- 右側の見出しクリックモーダル --}}
+        @include('components.UpdateContentModal')
 
-      var id = $(this).data('id');
-      $('#modal_id').val(id);
-    });
+        <a type="button" class="btn btn-primary mt-3 text-white" data-toggle="modal" data-target="#addHeadingModal">追加</a>
+        
+        {{-- 右側の見出し追加モーダル --}}
+        @include('components.AddContentModal')
 
- $('.editModal').on('click', function() {
-      var title = $(this).data('title');
-      $('#inputModal').val(title);
+      </div>
+    </div>
+  </div>
+</div>
 
-      var id_title = $(this).data('id_title');
-      $('#inputIdTitle').val(id_title);
-    });
+{{-- モータルへデータを送るjavascript --}}
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<script>
+// heading-modal（ループしているデータ）をクリックした時に
+$('.heading_modal').on('click', function() {
+  // このdata('heading')（data-title="ドルcontent->heading"）をheadingに代入
+  var heading = $(this).data('heading');
+  // このdata('body')（data-body="ドルcontent->body"）をtitleに代入
+  var body = $(this).data('body');
 
-    </script>
-  </body>
+  // '#inputHeading_modal'に対してtitleを埋める
+  $('#inputHeading_modal').val(heading);
+  // '#inputBody_modal'のvalueにbodyを設定する。
+  $('#inputBody_modal').val(body);
+
+  var id = $(this).data('id');
+  $('#modal_id').val(id);
+});
+
+$('.editModal').on('click', function() {
+  var title = $(this).data('title');
+  $('#inputModal').val(title);
+
+  var id_title = $(this).data('id_title');
+  $('#inputIdTitle').val(id_title);
+});
+
+</script>
+
 @endsection
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,34 +1,63 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+      <!-- CSRF Token -->
+      <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title')</title>
+      <title>@yield('title')</title>
 
-    <!-- Scripts -->
-    <!-- Laravel標準搭載のJavascript -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-    <!-- 下2行はbootstrapのJavascript -->
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+      <!-- Scripts -->
+      <!-- Laravel標準搭載のJavascript -->
+      <script src="{{ asset('js/app.js') }}" defer></script>
+      <!-- 下2行はbootstrapのJavascript -->
+      <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+      <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
-    <!-- bootstrapのcss -->
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+      <!-- Fonts -->
+      <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+      <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+      <!-- bootstrapのcss -->
+      <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/admin.css') }}" rel="stylesheet">
-    <link href="{{ asset('js/app.js') }}" rel="stylesheet">
-    <!-- <link rel="stylesheet" href="/css/app.css"> -->
-    <!-- <link rel="stylesheet" href="/css/admin.css">        -->
-  </head>
-    @yield('content')
+      <!-- Styles -->
+      <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+      <link href="{{ asset('css/admin.css') }}" rel="stylesheet">
+      <link href="{{ asset('js/app.js') }}" rel="stylesheet">
+      <!-- <link rel="stylesheet" href="/css/app.css"> -->
+      <!-- <link rel="stylesheet" href="/css/admin.css">        -->
+</head>
+<body>
+  <!-- ヘッダー -->
+  <nav class="navbar navbar-default bg-primary navbar-light">
+    <div class="container-fluid">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+        <a class="navbar-brand text-white" href="#">Todo</a>
+      </ul>
+      <ul class="navbar-nav">
+        <li class="nav-item navbar-light">
+          @if(Auth::check())
+            <span class="text-white">{{ Auth::user()-> name }}さんページ</span>
+            <a class="btn btn-primary" href="{{ route('logout') }}"
+              onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();">
+              ログアウト
+            </a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+              @csrf
+            </form>
+          @else
+            <a class="btn btn-primary" href="/login">ログイン</a>
+            <a class="btn btn-primary" href="/register">新規登録</a>
+          @endif
+        </li>
+      </ul>
+    </div>
+  </nav>
+  @yield('content')
+</body>
 </html>
