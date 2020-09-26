81b9c7df37a098a82a8b40bccdf1f3822fc0dcf8
Kent2525 <aibo396@yahoo.co.jp>
2020-09-27 00:57:39 +0900

    add image in top view

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -4,8 +4,13 @@
 @section('content')
 
 <h3 class="mt-5 text-center">仮トップページ</h3>
-<div class="text-center mt-3">
+<div class="text-center my-3">
   <a class="btn btn-primary" href="/admin/task">管理画面</a>
 </div>
+
+<div class="text-center" style="margin : 0 auto">
+  <img src="{{ asset('image/welcomeTop.jpg') }}" style="width: 85%;" alt="welcome Top">
+</div>
+
 
 @endsection
