88adbcd8ef64e9c0cc219ed5da08eaf8f5f9e7a5
Kent2525 <aibo396@yahoo.co.jp>
2020-09-26 15:57:55 +0900

    add button for top view

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -2,7 +2,10 @@
 @section('title','Todo一覧')
 
 @section('content')
+
 <h3 class="mt-5 text-center">仮トップページ</h3>
-<h5 class="mt-5 text-center">ログイン後、管理ページに飛ぶにはheaderの〇〇さんページをクリック</h5>
+<div class="text-center mt-3">
+  <a class="btn btn-primary" href="/admin/task">管理画面</a>
+</div>
 
 @endsection
