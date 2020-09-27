38bed25f69ee56656b6f48f1575454da1a59546e
Kent2525 <aibo396@yahoo.co.jp>
2020-09-27 19:47:08 +0900

    add button in top view

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -12,6 +12,13 @@
   <img src="{{ asset('image/welcomeTop.jpg') }}" style="width: 85%;" alt="welcome Top">
 </div>
 <h3 class="text-center" style="margin: 100px;">帰国時の予定を建てましょう</h3>
+<div class="text-center">
+  <button class="btn btn_tag btn-primary" style="border-radius: 20px;">#家族と会う</button>
+  <button class="btn btn_tag btn-primary" style="border-radius: 20px;">#買い物に行く</button>
+  <button class="btn btn_tag btn-primary" style="border-radius: 20px;">#お土産を買う</button>
+  <button class="btn btn_tag btn-primary" style="border-radius: 20px;">#日本食を食べる</button>
+</div>
+
 <div style="margin: 300px 0;">テスト</div>
 
 @endsection
