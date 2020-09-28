fdac713fed932cc65770ed53cfa58cf2c2264cfc
Kent2525 <aibo396@yahoo.co.jp>
2020-09-29 03:46:36 +0900

    made textarea in top view

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -9,14 +9,21 @@
   <div class="text-center" style="margin : 0 auto">
     <img src="{{ asset('image/welcomeTop5.jpg') }}" style="width: 85%;" alt="welcome Top">
   </div>
-  <h3 class="text-center" style="margin: 100px;">帰国時の予定を建てましょう</h3>
-  <div class="text-center">
-    <button class="btn btn_tag btn-primary" style="border-radius: 20px;">#家族と会う</button>
-    <button class="btn btn_tag btn-primary" style="border-radius: 20px;">#買い物に行く</button>
-    <button class="btn btn_tag btn-primary" style="border-radius: 20px;">#お土産を買う</button>
-    <button class="btn btn_tag btn-primary" style="border-radius: 20px;">#日本食を食べる</button>
-  </div>
+  <h3 class="text-center" style="margin-bottom: 80px;">帰国時の予定を建てましょう</h3>
+    <div class="text-center">
+      <button class="btn btn_tag btn-primary btn-lg" style="border-radius: 20px; margin-right: 30px;">#家族と会う</button>
+      <button class="btn btn_tag btn-primary btn-lg" style="border-radius: 20px; margin-right: 30px">#買い物に行く</button>
+      <button class="btn btn_tag btn-primary btn-lg" style="border-radius: 20px; margin-right: 30px">#お土産を買う</button>
+      <button class="btn btn_tag btn-primary btn-lg" style="border-radius: 20px; ">#日本食を食べる</button>
+    </div>
+      <div class="cp_iptxt">
+      <input type="text" placeholder="Todoを入力しましょう">
+        <i class="fas fa-list-ul" aria-hidden="true"></i>
+      </div>
 
+  <div class="text-center mt-4">
+    <textarea style="width: 750px; height: 210px; resize: none;"></textarea>
+  </div>
   <div style="margin: 300px 0;">テスト</div>
 </div>
 @endsection
