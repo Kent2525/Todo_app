b42840d80f353892594e35ce5bb9645790c6e0cc
KobayashiTomoya <kobayashi.tomoya@moneyforward.co.jp>
2020-10-03 11:44:59 +0900

    ログインにかかわらずセッションでの管理にする

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -15,38 +15,17 @@ class IndexController extends Controller
     }
     public function store(Request $request) {
         $tags = $request->input('tags');
-
+        
         // #区切りで送られてくるので、配列の形に分割する
         $tags = explode('#', $tags);
-
+        
         // 最初は空なので除く
         array_shift($tags);
         
-        // ログインしている場合
-        if( Auth::check() ) {
-            
-            // タイトルの新規作成
-            $title = new Title;
-            $title->user_id = Auth::id();
-            $title->title = '新規作成されました';
-            $title->save();
-
-            // コンテントの新規作成
-            foreach( $tags as $tag ) {
-                $content = new Content;
-                $content->heading = $tag;
-                $content->body = '';
-                $content->title_id = $title->id;
-                $content->save();
-            }
-
-            return redirect('/admin/title');
-        }else {
-            // セッションに保存する
-            session(['tags' => $tags]);
-
-            return redirect('/admin/title');
-        }
+        // セッションに保存する
+        session(['tags' => $tags]);
+        
+        return redirect('/admin/title');
         
     }
 }
