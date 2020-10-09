a5415f75df5441c330a8257d8c0527e808f2af1b
Kent2525 <aibo396@yahoo.co.jp>
2020-10-09 19:44:06 +0900

    route error of list creating

--- app/Http/Controllers/TopController.php
+++ app/Http/Controllers/TopController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Title;
+use App\Content;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TopController extends Controller
 {
@@ -13,7 +16,10 @@ class TopController extends Controller
 
     public function create()
     {
+        $content = Content::all();
         return view('top');  
-    }
-
+        unset($form['_token']);
+        $content->fill($form)->save();       
+        return back();
+    }   
 }
