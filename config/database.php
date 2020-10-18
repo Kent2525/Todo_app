5a67c66cb12a26374ec5d4ee194a2fb27ade9647
Kent2525 <aibo396@yahoo.co.jp>
2020-10-18 11:27:06 +0900

    test circleCI15

--- config/database.php
+++ config/database.php
@@ -145,20 +145,19 @@ return [
     ],
 
     'connections' => [
-        'circleci' => [
+        'circle_test' => [
             'driver' => 'mysql',
             'host' => '127.0.0.1',
             'port' => '3306',
             'database' => 'circle_test',
-            'username' => 'root',
+            'username' => 'ubuntu',
             'password' => '',
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
-            'prefix_indexes' => true,
             'strict' => true,
-            'engine' => 'InnoDB ROW_FORMAT=DYNAMIC',
+            'engine' => null,
         ],
-    ],
+    ]
 
 ];
