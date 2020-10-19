869b9dde766427cff754785416e19d5f10b0775d
Kent2525 <aibo396@yahoo.co.jp>
2020-10-20 01:17:32 +0900

    feature test for route

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -15,6 +15,30 @@ class ExampleTest extends TestCase
     public function testBasicTest()
     {
         $this->get('/')->assertStatus(200);
+        $this->get('/todo')->assertStatus(302);
+        $this->get('/todo/show/1')->assertStatus(302);
+        $this->get('/todo/addTitle')->assertStatus(302);
+        $this->get('/todo/1/delete')->assertStatus(302);
+        $this->get('/todo/editTitle')->assertStatus(302);
+        $this->get('/todo/content')->assertStatus(302);
+        $this->get('/todo/addContent/1')->assertStatus(302);
+        $this->get('/login')->assertStatus(200);
+        $this->get('/register')->assertStatus(200);
+        $this->get('/easyLogin')->assertStatus(200);
+        $this->get('/home')->assertStatus(200);
+        
+        $this->get('/')->assertSeeText('おかえりTodo');
+        $this->get('/')->assertSeeText('TOPページ');
+        // JS onclickの文章
+        $this->get('/')->assertSeeText('家族と会う');
+        $this->get('/')->assertSee('</div>');
+
+        $response = $this->get(route('todo.show', ['id' => 1]));
+        $response->assertStatus(302);
+        $response = $this->post('/todo/create', ['title' => 'sample']);
+        $response->assertStatus(302);
+        $response = $this->post('/todo/editTitle', ['title' => 'sample']);
+        $response->assertStatus(302);
 
         // $response = $this->get('/todo');
         // $response->assertStatus(302);
